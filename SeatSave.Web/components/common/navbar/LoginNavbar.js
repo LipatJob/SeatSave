1a36771271c637b01d936930c7ea14538d81ca80
charmaine <charmaine.rabano@gmail.com>
2022-04-20 18:59:45 +0800

    Implemented login navbar

---
+++ SeatSave.Web/components/common/navbar/LoginNavbar.js
@@ -0,0 +1,11 @@
+import NavbarLogo from './NavbarLogo';
+
+export default function LoginNavbar() {
+  return (
+    <div className='flex flex-col justify-center bg-dusk-blue h-20'>
+      <div className='page-container w-full'>
+        <NavbarLogo />
+      </div>
+    </div>
+  );
+}

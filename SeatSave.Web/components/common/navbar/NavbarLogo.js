0015bdff21a25d703906db6808ab3ed6813aeead
charmaine <charmaine.rabano@gmail.com>
2022-04-20 19:38:53 +0800

    Implemented custom form styles

--- SeatSave.Web/components/common/navbar/NavbarLogo.js
+++ SeatSave.Web/components/common/navbar/NavbarLogo.js
@@ -3,10 +3,10 @@ import Image from 'next/image';
 export default function NavbarLogo() {
   return (
     <>
-      <div className='lg:hidden flex'>
+      <div className='lg:hidden mt-2'>
         <Image src='/logo-icon.png' width={54} height={54} />
       </div>
-      <div className='hidden lg:block'>
+      <div className='hidden lg:block mt-2'>
         <Image src='/logo.png' width={246} height={53} />
       </div>
     </>

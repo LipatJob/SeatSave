d67f40f4aac53c012005376193554a8ea5d6f605
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-20 23:09:04 +0800

    register page decoration image now changes based on form part

--- SeatSave.Web/pages/register.js
+++ SeatSave.Web/pages/register.js
@@ -7,7 +7,6 @@ import VisitorInformationForm from '../components/register/VisitorInformationFor
 
 export default function Register() {
   const [formData, setFormData] = useState({});
-
   const [formPart, setFormPart] = useState(0);
 
   const goToNextFormPart = (accountInformation) => {
@@ -32,13 +31,24 @@ export default function Register() {
   return (
     <div className='sm:grid sm:grid-cols-2 page-container sm:gap-x-20'>
       <div className='hidden sm:block'>
-        <Image
-          src='/LoginDecoration.svg'
-          className='w-full h-auto'
-          layout='responsive'
-          width={500}
-          height={500}
-        />
+        {formPart === 0 && (
+          <Image
+            src='/RegisterPart1.svg'
+            className='w-full h-auto'
+            layout='responsive'
+            width={500}
+            height={500}
+          />
+        )}
+        {formPart === 1 && (
+          <Image
+            src='/RegisterPart2.svg'
+            className='w-full h-auto'
+            layout='responsive'
+            width={500}
+            height={500}
+          />
+        )}
       </div>
       {formPart === 0 && <AccountInformationForm onSubmit={goToNextFormPart} />}
       {formPart === 1 && (

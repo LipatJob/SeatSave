098d9ea1b26212ed410bb62a05d7c5177ab7ef1b
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-20 18:49:26 +0800

    Implemented register form design - implemented design of account information form - implemented design of visitor information forms

---
+++ SeatSave.Web/components/register/AccountInformationForm.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Link from 'next/link';
+import Button from '../common/buttons/Button';
+
+export default function AccountInformationForm() {
+  return (
+    <form className='flex flex-col items-center'>
+      <h1 className='mb-16 text-center text-dusk-blue'>Create Your Account</h1>
+      <div className='sm:max-w-md'>
+        <div className='flex flex-col items-center mb-12 gap-y-7'>
+          <div className='grid w-full grid-cols-2 gap-4'>
+            <div className='w-full'>
+              <p className='font-light body-small'>First Name</p>
+              <input
+                id='firstname'
+                type='text'
+                name='firstname'
+                placeholder='First Name'
+                className='w-full p-2 py-2.5 border border-solid border-dawn body-normal'
+                onChange={() => false}
+              />
+            </div>
+            <div className='w-full'>
+              <p className='font-light body-small'>Last Name</p>
+              <input
+                id='lastname'
+                type='text'
+                name='lastname'
+                placeholder='Last Name'
+                className='w-full p-2 py-2.5 border border-solid border-dawn body-normal'
+                onChange={() => false}
+              />
+            </div>
+          </div>
+          <div className='w-full'>
+            <p className='font-light body-small'>Email</p>
+            <input
+              id='email'
+              type='email'
+              name='email'
+              placeholder='student@live.mcl.edu.ph'
+              className='w-full p-2 py-2.5 border border-solid border-dawn body-normal'
+              onChange={() => false}
+            />
+          </div>
+          <div className='w-full'>
+            <p className='font-light body-small'>Password</p>
+            <input
+              id='password'
+              type='password'
+              name='password'
+              placeholder='*******'
+              className='w-full p-2 py-2.5 border border-solid border-dawn body-normal'
+              onChange={() => false}
+            />
+          </div>
+        </div>
+        <div className='flex flex-col items-center text-center'>
+          <Button text='CONTINUE' className='w-full py-3.5 mb-6'></Button>
+          <p className='body-small'>
+            Already have an account?{' '}
+            <Link href='/login'>
+              <span className='font-bold text-bluish body-small'>LOG IN</span>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </form>
+  );
+}
---
+++ SeatSave.Web/components/register/VisitorInformationForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Button from '../common/buttons/Button';
+import StaffInformationForm from './StaffInformationForm';
+import FacultyInformationForm from './FacultyInformationForm';
+import StudentInformationForm from './StudentInformationForm';
+import { BiArrowBack } from 'react-icons/bi';
+
+export default function VisitorInformationForm() {
+  const [visitorType, setVisitorType] = useState('');
+  const visitorForms = {
+    Student: <StudentInformationForm />,
+    Faculty: <FacultyInformationForm />,
+    Staff: <StaffInformationForm />,
+  };
+
+  return (
+    <form className='flex flex-col items-center'>
+      <div className='w-full sm:max-w-md'>
+        <div className='flex flex-col items-center mb-12 gap-y-7'>
+          <button className='mr-auto text-bluish' type='button'>
+            <BiArrowBack className='text-[40px]' />
+          </button>
+          <div className='w-full'>
+            <p className='font-light body-small'>Type of Visitor</p>
+            <select
+              name='visitorType'
+              id='visitorType'
+              className='w-full p-2 py-2.5 border border-solid border-dawn body-normal'
+              onChange={(event) => setVisitorType(event.target.value)}
+            >
+              <option value='Student'>Student</option>
+              <option value='Faculty'>Faculty</option>
+              <option value='Staff'>Staff</option>
+            </select>
+          </div>
+          {visitorForms[visitorType]}
+        </div>
+        <div className='flex flex-col items-center text-center'>
+          {visitorType in visitorForms && (
+            <Button text='CONTINUE' className='w-full py-3.5 mb-6'>
+              CREATE ACCOUNT
+            </Button>
+          )}
+        </div>
+      </div>
+    </form>
+  );
+}

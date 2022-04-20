02cf411f1735ed3878b405078f01db741c1c3732
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-20 20:41:31 +0800

    fixed styles

--- SeatSave.Web/components/register/StaffInformationForm.jsx
+++ SeatSave.Web/components/register/StaffInformationForm.jsx
@@ -2,27 +2,23 @@ import React from 'react';
 
 export default function StaffInformationForm() {
   return (
-    <>
-      <div className='w-full'>
-        <p className='font-light body-small'>Office</p>
-        <select
-          name='staffOffice'
-          id='staffOffice'
-          className='w-full p-2 py-2.5 border border-solid border-dawn body-normal'
-          onChange={() => false}
-        >
-          <option value='Office of the President'>
-            Office of the President
-          </option>
-          <option value='Office of the Executive Vice President'>
-            Office of the Executive Vice President
-          </option>
-          <option value='OSVPAL'>OSVPAL</option>
-          <option value='OVPAA'>OVPAA</option>
-          <option value='OVPF'>OVPF</option>
-          <option value='OAVPAS'>I-OAVPAS</option>
-        </select>
-      </div>
-    </>
+    <div className='w-full'>
+      <p className='font-light body-small'>Office</p>
+      <select
+        name='staffOffice'
+        id='staffOffice'
+        className='w-full p-2 py-2.5 border border-solid border-dawn body-normal'
+        onChange={() => false}
+      >
+        <option value='Office of the President'>Office of the President</option>
+        <option value='Office of the Executive Vice President'>
+          Office of the Executive Vice President
+        </option>
+        <option value='OSVPAL'>OSVPAL</option>
+        <option value='OVPAA'>OVPAA</option>
+        <option value='OVPF'>OVPF</option>
+        <option value='OAVPAS'>I-OAVPAS</option>
+      </select>
+    </div>
   );
 }

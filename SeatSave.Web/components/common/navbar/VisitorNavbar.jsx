3d590efabdbb4f496ef43ab0c06bbec2784b2a76
charmaine <charmaine.rabano@gmail.com>
2022-04-21 20:37:07 +0800

    Implemented Desktop Menu

--- SeatSave.Web/components/common/navbar/VisitorNavbar.jsx
+++ SeatSave.Web/components/common/navbar/VisitorNavbar.jsx
@@ -1,20 +1,27 @@
 import NavbarLogo from './NavbarLogo';
+import DesktopMenu from './DesktopMenu';
 import AccountButton from './AccountButton';
 import MobileMenu from './MobileMenu';
-import Button from '../buttons/Button';
 
-export default function LibrarianNavbar() {
+const visitorLinks = [
+  {
+    name: 'View Booking Details',
+    path: '/',
+  },
+  {
+    name: 'Book A Seat',
+    path: '/book-a-seat',
+  },
+];
+
+export default function VisitorNavbar() {
   return (
     <div className='flex flex-col justify-center h-20 bg-dusk-blue'>
       <div className='flex flex-row items-center justify-between w-full text-white page-container'>
         <div className='flex flex-row items-center'>
           <NavbarLogo />
           <div className='hidden ml-12 lg:block'>
-            <div className='flex flex-row items-center gap-10 body-small'>
-              <Button text='Check In / Out' />
-              <a href='/librarian/view-bookings'>View Bookings</a>
-              <a href=''>Edit Library Settings</a>
-            </div>
+            <DesktopMenu links={visitorLinks} />
           </div>
         </div>
         <div className='hidden lg:block'>

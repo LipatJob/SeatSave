83fae21693485f8d244f7fd9a85e7e3c30370bc0
Michiru Suzumiya <mamamauag@live.mcl.edu.ph>
2022-04-21 08:28:28 +0800

    scrollable date and time

--- SeatSave.Web/pages/book-a-seat.js
+++ SeatSave.Web/pages/book-a-seat.js
@@ -1,8 +1,8 @@
 export default function BookASeat() {
   return (
-    <div class='bg-slate-200 min-h-screen min-w-screen flex items-center justify-center'>
-      <div class='bg-slate-300 w-3/4 min-h-screen m-20 rounded-md'>
-        <h1 class='text-6xl pl-48 pt-32 font-bold'>Book a Seat</h1>
+    <div class='min-h-screen min-w-screen flex items-center justify-center'>
+      <div class='w-3/4 min-h-screen m-20 rounded-md'>
+        <h1 class='text-6xl pl-48 pt-20 font-bold'>Book a Seat</h1>
         <h2 class='text-2xl pl-48 pt-5'>
           Center for Learning and Information Resources - Einstein Bldg.
         </h2>
@@ -12,13 +12,30 @@ export default function BookASeat() {
         </div>
 
         <div class='flex justify-center'>
-          <div class='py-6'>
-            <button class='bg-pearl-bush rounded-md w-40 mx-5'>
-              <h3 class='text-2xl p-8'>April 21, 2022</h3>
+          <div class='py-6 w-3/4 flex flex-nowrap overflow-x-auto'>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
             </button>
-
-            <button class='bg-pearl-bush rounded-md w-40 mx-5'>
-              <h3 class='text-2xl p-8'>April 22, 2022</h3>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
+            </button>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
+            </button>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
+            </button>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
+            </button>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
+            </button>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
+            </button>
+            <button class='bg-pearl-bush rounded-md w-56 mx-5'>
+              <h5 class='text-2xl py-4 px-16'>April 21, 2022</h5>
             </button>
           </div>
         </div>
@@ -28,12 +45,52 @@ export default function BookASeat() {
         </div>
 
         <div class='flex justify-center'>
-          <div class='py-6'>
+          <div class='py-6 w-3/4 flex flex-nowrap overflow-x-auto'>
             <button class='bg-pearl-bush rounded-md w-30 mx-5'>
               <h3 class='text-2xl px-12 py-4'>
                 3:00
                 <br />
                 4:00
+              </h3>
+            </button>
+
+            <button class='bg-pearl-bush rounded-md w-30 mx-5'>
+              <h3 class='text-2xl px-12 py-4'>
+                4:00
+                <br />
+                5:00
+              </h3>
+            </button>
+
+            <button class='bg-pearl-bush rounded-md w-30 mx-5'>
+              <h3 class='text-2xl px-12 py-4'>
+                4:00
+                <br />
+                5:00
+              </h3>
+            </button>
+
+            <button class='bg-pearl-bush rounded-md w-30 mx-5'>
+              <h3 class='text-2xl px-12 py-4'>
+                4:00
+                <br />
+                5:00
+              </h3>
+            </button>
+
+            <button class='bg-pearl-bush rounded-md w-30 mx-5'>
+              <h3 class='text-2xl px-12 py-4'>
+                4:00
+                <br />
+                5:00
+              </h3>
+            </button>
+
+            <button class='bg-pearl-bush rounded-md w-30 mx-5'>
+              <h3 class='text-2xl px-12 py-4'>
+                4:00
+                <br />
+                5:00
               </h3>
             </button>
 
@@ -54,36 +111,44 @@ export default function BookASeat() {
         <div class='flex justify-center'>
           <div class='bg-pearl-bush py-6 w-3/6 h-96 m-6 rounded-lg overflow-x-auto'>
             <div class='grid grid-cols-2'>
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E1</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 1</h2>
+                <h2 class='text-2xl text-white pb-3'>E1</h2>
               </button>
 
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E2</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 2</h2>
+                <h2 class='text-2xl text-white pb-3'>E2</h2>
               </button>
 
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E3</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 3</h2>
+                <h2 class='text-2xl text-white pb-3'>E3</h2>
               </button>
 
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E4</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 4</h2>
+                <h2 class='text-2xl text-white pb-3'>E4</h2>
               </button>
 
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E5</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 5</h2>
+                <h2 class='text-2xl text-white pb-3'>E5</h2>
               </button>
 
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E6</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 6</h2>
+                <h2 class='text-2xl text-white pb-3'>E6</h2>
               </button>
 
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E7</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 7</h2>
+                <h2 class='text-2xl text-white pb-3'>E7</h2>
               </button>
 
-              <button class='bg-bluish rounded-md m-5'>
-                <h2 class='text-2xl p-8'>Einstein - E8</h2>
+              <button class='bg-dusk-blue rounded-md m-5'>
+                <h2 class='text-2xl text-white px-3 pt-3'>Einstein - 8</h2>
+                <h2 class='text-2xl text-white pb-3'>E8</h2>
               </button>
             </div>
           </div>
@@ -91,7 +156,12 @@ export default function BookASeat() {
 
         <br />
         <br />
-        <br />
+
+        <div class='flex justify-center'>
+          <button class='bg-bluish rounded-md m-5'>
+            <h2 class='text-2xl font-bold text-white p-4'>Book your seat!</h2>
+          </button>
+        </div>
       </div>
     </div>
   );

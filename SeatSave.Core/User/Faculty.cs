023c790116fd97128a249c9d01ddb4d78de4fa09
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-25 13:26:46 +0800

    added possible values

--- SeatSave.Core/User/Faculty.cs
+++ SeatSave.Core/User/Faculty.cs
@@ -3,6 +3,18 @@
     public class Faculty : Visitor
     {
         public const string UserType = "Faculty";
+
+        public static IEnumerable<string> FacultyOffices = new HashSet<string>
+        {
+            "College of Arts and Science (CAS)",
+            "College of Computer and Information Science (CCIS)",
+            "E.T. Tuchengco College of Business (ETYCB)",
+            "Institute for Excellence in Continuing Education and Lifelong Learning (I-ExCELL)",
+            "Mapúa Institute of Technology at Laguna (MITL)",
+            "Mapúa-PTC College of Maritime Education and Training (Mapúa-PTC CMET)",
+            "MCL Senior High School (MCL-SHS)",
+        };
+
         public Faculty() : base(UserType)
         {
         }
--- SeatSave.Core/User/Student.cs
+++ SeatSave.Core/User/Student.cs
@@ -5,6 +5,56 @@
     {
         public const string UserType = "Student";
 
+        public static IEnumerable<string> YearLevels = new HashSet<string>{
+            "First Year",
+            "Second Year",
+            "Third Year",
+            "Fourth Year",
+            "Fifth Year",
+        };
+
+        public static IEnumerable<string> GradeLevels = new HashSet<string>{
+            "Grade 11",
+            "Grade 12",
+        };
+
+        public static Dictionary<string, HashSet<string>> StudentValues = new Dictionary<string, HashSet<string>>{
+            {"MITL", new HashSet<string>{
+                "B.S. ARCHITECTURE",
+                "B.S. CHEMICAL ENGINEERING",
+                "B.S. CIVIL ENGINEERING",
+                "B.S. COMPUTER ENGINEERING",
+                "B.S. ELECTRICAL ENGINEERING",
+                "B.S. ELECTRONICS ENGINEERING",
+                "B.S. INDUSTRIAL ENGINEERING",
+                "B.S. MECHANICAL ENGINEERING",
+            }},
+            {"ETCYB", new HashSet<string>{
+                "B.S. ACCOUNTANCY",
+                "B.S. BUSINESS ADMINISTRATION",
+                "B.S. TOURISM MANAGEMENT",
+            }},
+             {"CAS", new HashSet<string>{
+                "B.A. COMMUNICATION",
+                "B. MULTIMEDIA ARTS",
+                "B.S. PSYCHOLOGY",
+            }},
+            {"CCIS", new HashSet<string>{
+                "B.S. COMPUTER SCIENCE",
+                "B.S. INFORMATION TECHNOLOGY",
+            }},
+            {"CMET", new HashSet<string>{
+                "B.S. MARINE ENGINEERING",
+                "B.S. MARINE TRANSPORTATION",
+            }},
+            {"SHS", new HashSet<string>{
+                "STEM",
+                "ABM",
+                "HUMSS",
+                "ICT",
+            }},
+    };
+
         public Student() : base(UserType)
         {
         }

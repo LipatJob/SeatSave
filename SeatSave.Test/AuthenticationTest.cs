14fe5fc399209bcc0a743470caf5561c4a926d08
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-26 10:55:00 +0800

    refactored authentication controller

---
+++ SeatSave.Test/AuthenticationTest.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace SeatSave.Test
+{
+    public class AuthenticationTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void LoginWithValidCredentialsSucceeds()
+        {
+            
+        }
+
+        [Fact]
+        public void LoginWithUnregisteredEmailFails()
+        {
+
+        }
+
+        [Fact]
+        public void LoginWithIncorrectPasswordFails()
+        {
+
+        }
+
+        [Fact]
+        public void LoginWithMissingPasswordFails()
+        {
+
+        }
+
+        [Fact]
+        public void LoginWithMissingEmailFails()
+        {
+
+        }
+    }
+}

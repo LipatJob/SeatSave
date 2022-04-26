281e24beac590c3a7e32d76f3729a7a7ba97e4ee
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-26 22:17:48 +0800

    Started with regular weekly availability

--- SeatSave.Core/Schedule/ScheduleModel.cs
+++ SeatSave.Core/Schedule/ScheduleModel.cs
@@ -4,6 +4,15 @@
     {
         /* TODO: Properties */
 
+        IEnumerable<RegularDayOfWeekAvailability> regularDaySchedule;
+        IEnumerable<SpecificDateAvailability> specifcDaySchedule;
+
+        public ScheduleModel(IEnumerable<RegularDayOfWeekAvailability> regularDaySchedule, IEnumerable<SpecificDateAvailability> specifcDaySchedule)
+        {
+            this.regularDaySchedule = regularDaySchedule;
+            this.specifcDaySchedule = specifcDaySchedule;
+        }
+
         public bool IsAvailable(DateTime date, Period period) { throw new NotImplementedException("TODO"); }
         public IList<DateOnly> GetAvailableDays() { throw new NotImplementedException("TODO"); }
         public IList<Period> GetAvailablePeriods(DateTime date) { throw new NotImplementedException("TODO"); }

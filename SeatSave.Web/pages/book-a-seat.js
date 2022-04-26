ed716f269c1cac3ca23518f8295535fbed461cfe
Michiru Suzumiya <mamamauag@live.mcl.edu.ph>
2022-04-26 08:06:53 +0800

    - implemented API for dates - the dates ranges from the current day to the 21th day

--- SeatSave.Web/pages/book-a-seat.js
+++ SeatSave.Web/pages/book-a-seat.js
@@ -1,70 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
 
-export default function BookASeat() {
+export default function BookASeat({ availableDays }) {
   function submitBooking(e) {
     e.preventDefault();
     console.log('Submitted!');
   }
 
+  const day = availableDays.map((availableDay) => new Date(availableDay));
   return (
-    <div className='min-h-screen min-w-screen flex items-center justify-center'>
+    <div className='flex items-center justify-center min-h-screen min-w-screen'>
       <div className='w-3/4 min-h-screen m-20 rounded-md'>
-        <h1 className='text-center lg:text-center md:text-center xl:text-left xl:pl-48 pt-10 font-bold'>
+        <h1 className='pt-10 font-bold text-center lg:text-center md:text-center xl:text-left xl:pl-48'>
           Book a Seat
         </h1>
-        <h4 className='text-center lg:text-center md:text-center xl:text-left xl:pl-48 pt-5 pb-10'>
+        <h4 className='pt-5 pb-10 text-center lg:text-center md:text-center xl:text-left xl:pl-48'>
           Center for Learning and Information Resources - Einstein Bldg.
         </h4>
 
         <div className='flex justify-center'>
-          <h5 className='font-bold pt-20'>Select your date</h5>
+          <h5 className='pt-20 font-bold'>Select your date</h5>
         </div>
 
         <div className='flex justify-center'>
-          <div className='py-6 w-3/4 flex flex-nowrap overflow-x-auto'>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
-            <button className='bg-pearl-bush rounded-md w-56 mx-5'>
-              <h5 className='py-4 px-16'>April 21, 2022</h5>
-            </button>
+          <div className='flex w-3/4 py-6 overflow-x-auto flex-nowrap'>
+            {day.map((availableDay) => (
+              <button className='w-56 mx-5 rounded-md bg-pearl-bush '>
+                <h5 className='px-16 py-4'>
+                  {moment(availableDay).format('MMMM D, YYYY')}
+                </h5>
+              </button>
+            ))}
           </div>
         </div>
 
         <div className='flex justify-center'>
-          <h5 className='font-bold pt-20'>Select your time</h5>
+          <h5 className='pt-20 font-bold'>Select your time</h5>
         </div>
 
         <div className='flex justify-center'>
-          <div className='py-6 w-3/4 flex flex-nowrap overflow-x-auto'>
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+          <div className='flex w-3/4 py-6 overflow-x-auto flex-nowrap'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -72,7 +48,7 @@ export default function BookASeat() {
               </h5>
             </button>
 
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -80,7 +56,7 @@ export default function BookASeat() {
               </h5>
             </button>
 
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -88,7 +64,7 @@ export default function BookASeat() {
               </h5>
             </button>
 
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -96,7 +72,7 @@ export default function BookASeat() {
               </h5>
             </button>
 
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -104,7 +80,7 @@ export default function BookASeat() {
               </h5>
             </button>
 
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -112,7 +88,7 @@ export default function BookASeat() {
               </h5>
             </button>
 
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -120,7 +96,7 @@ export default function BookASeat() {
               </h5>
             </button>
 
-            <button className='bg-pearl-bush rounded-md w-30 mx-5'>
+            <button className='mx-5 rounded-md bg-pearl-bush w-30'>
               <h5 className='px-12 py-4'>
                 3:00
                 <br />
@@ -131,50 +107,53 @@ export default function BookASeat() {
         </div>
 
         <div className='flex justify-center'>
-          <h5 className='font-bold pt-20'>Pick your seat</h5>
+          <h5 className='pt-20 font-bold'>Pick your seat</h5>
         </div>
 
         <div className='flex justify-center'>
-          <div className='bg-pearl-bush py-6 w-3/4 sm:w-3/6 h-96 m-6 rounded-lg overflow-x-auto'>
+          <div className='w-3/4 py-6 m-6 overflow-x-auto rounded-lg bg-pearl-bush sm:w-3/6 h-96'>
             <div className='grid grid-cols-1 sm:grid-cols-2'>
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button
+                className='m-5 rounded-md bg-dusk-blue'
+                onClick={() => setShowModal(true)}
+              >
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
 
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button className='m-5 rounded-md bg-dusk-blue'>
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
 
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button className='m-5 rounded-md bg-dusk-blue'>
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
 
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button className='m-5 rounded-md bg-dusk-blue'>
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
 
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button className='m-5 rounded-md bg-dusk-blue'>
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
 
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button className='m-5 rounded-md bg-dusk-blue'>
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
 
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button className='m-5 rounded-md bg-dusk-blue'>
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
 
-              <button className='bg-dusk-blue rounded-md m-5'>
-                <h5 className='text-white px-3 pt-3'>Einstein - 1</h5>
-                <h5 className='text-white pb-3'>E1</h5>
+              <button className='m-5 rounded-md bg-dusk-blue'>
+                <h5 className='px-3 pt-3 text-white'>Einstein - 1</h5>
+                <h5 className='pb-3 text-white'>E1</h5>
               </button>
             </div>
           </div>
@@ -189,12 +168,23 @@ export default function BookASeat() {
             type='button'
             className='px-4 py-2 text-xl font-bold text-white uppercase rounded-lg bg-bluish hover:bg-dusk-blue'
           >
-            <h5 className='font-bold text-white p-4'>Book your seat!</h5>
+            <h5 className='p-4 font-bold text-white'>Book your seat!</h5>
           </button>
         </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.API_URL}/Api/Schedule`);
+  const availableDays = await res.json();
+
+  return {
+    props: {
+      availableDays,
+    },
+  };
 }
 
 BookASeat.page = 'BookASeat';

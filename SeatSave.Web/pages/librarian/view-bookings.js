48342efdc2a14ba941b97bd60a05a52d9f8d1fef
charmaine <charmaine.rabano@gmail.com>
2022-04-27 08:42:55 +0800

    Implemented table and pagination functionalities

--- SeatSave.Web/pages/librarian/view-bookings.js
+++ SeatSave.Web/pages/librarian/view-bookings.js
@@ -2,12 +2,80 @@ import React from 'react';
 import ViewBookingsForm from '../../components/librarian/view-bookings/ViewBookingsForm';
 import ViewBookingsTable from '../../components/librarian/view-bookings/ViewBookingsTable';
 
+// dummy data
+const bookingsData = [
+  {
+    id: 'XkeRvmSioV1',
+    seat: 'R01',
+    visitor: {
+      name: 'John Doe',
+      email: 'johndoe@live.mcl.edu.ph',
+    },
+    date: 'April 12, 2022',
+    period: {
+      timeStart: '10:00 am',
+      timeEnd: '11:00 am',
+    },
+    status: 'Completed',
+    checkedIn: '10:02 am',
+    checkedOut: '10:50 am',
+  },
+  {
+    id: 'XkeRvmSioV2',
+    seat: 'R01',
+    visitor: {
+      name: 'John Doe',
+      email: 'johndoe@live.mcl.edu.ph',
+    },
+    date: 'April 12, 2022',
+    period: {
+      timeStart: '10:00 am',
+      timeEnd: '11:00 am',
+    },
+    status: 'Pending',
+    checkedIn: '',
+    checkedOut: '',
+  },
+  {
+    id: 'XkeRvmSioV3',
+    seat: 'R01',
+    visitor: {
+      name: 'John Doe',
+      email: 'johndoe@live.mcl.edu.ph',
+    },
+    date: 'April 12, 2022',
+    period: {
+      timeStart: '10:00 am',
+      timeEnd: '11:00 am',
+    },
+    status: 'Cancelled',
+    checkedIn: '',
+    checkedOut: '',
+  },
+  {
+    id: 'XkeRvmSioV4',
+    seat: 'R01',
+    visitor: {
+      name: 'John Doe',
+      email: 'johndoe@live.mcl.edu.ph',
+    },
+    date: 'April 12, 2022',
+    period: {
+      timeStart: '10:00 am',
+      timeEnd: '11:00 am',
+    },
+    status: 'Checked In',
+    checkedIn: '10:02 am',
+    checkedOut: '',
+  },
+];
+
 export default function ViewBookings() {
   return (
     <div className='page-container'>
       <h1>View Bookings</h1>
       <ViewBookingsForm />
-      <ViewBookingsTable />
+      <ViewBookingsTable bookings={bookingsData} />
     </div>
   );
 }

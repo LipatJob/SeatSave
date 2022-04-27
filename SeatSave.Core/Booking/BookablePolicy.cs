e275758571bc440dbca3abcb4281531edcdeeb85
Michiru Suzumiya <mamamauag@live.mcl.edu.ph>
2022-04-27 09:57:06 +0800

    API logic

--- SeatSave.Core/Booking/BookablePolicy.cs
+++ SeatSave.Core/Booking/BookablePolicy.cs
@@ -5,6 +5,13 @@ namespace SeatSave.Core.Booking
 {
     public class BookablePolicy
     {
-        public bool IsSatisfied(DateTime date, Period period, SeatModel seat) { throw new NotImplementedException("TODO"); }
+        public bool IsSatisfied(DateTime date, Period period, SeatModel seat) { 
+            // Bookable if:
+            // 1. Check schedule if date is available
+            // 2. Check schedule if period on the given date is available
+            // 3. Check if there no booking on specified seat on specified date and period
+            
+            throw new NotImplementedException("TODO"); 
+        }
     }
 }

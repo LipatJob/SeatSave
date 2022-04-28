7e60657285983c0d6ec9e5c7b79e6738d8487f98
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-28 22:44:03 +0800

    tweak styles a bit

--- SeatSave.Web/components/librarian/manage-seat/SeatInformation.jsx
+++ SeatSave.Web/components/librarian/manage-seat/SeatInformation.jsx
@@ -1,5 +1,5 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import React, { useState, useEffect } from 'react';
+import { Field, Form, Formik } from 'formik';
+import React from 'react';
 
 export default function SeatInformation({ seatData }) {
   return (
@@ -7,58 +7,56 @@ export default function SeatInformation({ seatData }) {
       <Formik initialValues={seatData} enableReinitialize>
         {() => (
           <Form>
-            <div className='sm:max-w-md'>
-              <div className='flex flex-col items-center mb-12 gap-y-7'>
-                <div className='w-full'>
-                  <div>
-                    Seat ID
-                    <Field
-                      type='text'
-                      id='inputSeatID'
-                      name='inputSeatID'
-                      placeholder='Text'
-                      className='flex bg-iron'
-                      disabled
-                    />
-                  </div>
-                  <div>
-                    Name
-                    <Field
-                      type='text'
-                      id='name'
-                      name='name'
-                      placeholder='Enter Seat Name'
-                      className='flex w-full'
-                    />
-                  </div>
-                  <div>
-                    Type
-                    <Field
-                      as='select'
-                      id='type'
-                      name='type'
-                      className='flex w-full'
-                      placeholder='Select Seat Type'
-                    >
-                      <option value='default' disabled hidden>
-                        Select Seat Type
-                      </option>
-                      <option value='1'>Carrel Desk</option>
-                      <option value='2'>Carrel Desk with Outlet</option>
-                    </Field>
-                  </div>
-                  <div>
-                    Description
-                    <Field
-                      as='textarea'
-                      type='textarea'
-                      id='description'
-                      name='description'
-                      className='flex w-full h-32'
-                      style={{ resize: 'none' }}
-                      placeholder='Enter Seat Description'
-                    />
-                  </div>
+            <div className='flex flex-col items-center w-full mb-12 gap-y-7'>
+              <div className='w-full'>
+                <div>
+                  <p>Seat ID</p>
+                  <Field
+                    type='text'
+                    id='inputSeatID'
+                    name='inputSeatID'
+                    placeholder='Text'
+                    className='bg-iron'
+                    disabled
+                  />
+                </div>
+                <div>
+                  Name
+                  <Field
+                    type='text'
+                    id='name'
+                    name='name'
+                    placeholder='Enter Seat Name'
+                    className='flex w-full'
+                  />
+                </div>
+                <div>
+                  Type
+                  <Field
+                    as='select'
+                    id='type'
+                    name='type'
+                    className='flex w-full'
+                    placeholder='Select Seat Type'
+                  >
+                    <option value='default' disabled hidden>
+                      Select Seat Type
+                    </option>
+                    <option value='1'>Carrel Desk</option>
+                    <option value='2'>Carrel Desk with Outlet</option>
+                  </Field>
+                </div>
+                <div>
+                  Description
+                  <Field
+                    as='textarea'
+                    type='textarea'
+                    id='description'
+                    name='description'
+                    className='flex w-full h-32'
+                    style={{ resize: 'none' }}
+                    placeholder='Enter Seat Description'
+                  />
                 </div>
               </div>
             </div>

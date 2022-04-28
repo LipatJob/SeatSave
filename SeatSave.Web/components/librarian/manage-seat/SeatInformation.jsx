cb7d1fd75462c0832317b8c4693f599d5fecdbfa
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-28 22:45:15 +0800

    tweaked UI

--- SeatSave.Web/components/librarian/manage-seat/SeatInformation.jsx
+++ SeatSave.Web/components/librarian/manage-seat/SeatInformation.jsx
@@ -6,58 +6,56 @@ export default function SeatInformation({ seatData }) {
     <form>
       <Formik initialValues={seatData} enableReinitialize>
         {() => (
-          <Form>
-            <div className='flex flex-col items-center w-full mb-12 gap-y-7'>
-              <div className='w-full'>
-                <div>
-                  <p>Seat ID</p>
-                  <Field
-                    type='text'
-                    id='inputSeatID'
-                    name='inputSeatID'
-                    placeholder='Text'
-                    className='bg-iron'
-                    disabled
-                  />
-                </div>
-                <div>
-                  Name
-                  <Field
-                    type='text'
-                    id='name'
-                    name='name'
-                    placeholder='Enter Seat Name'
-                    className='flex w-full'
-                  />
-                </div>
-                <div>
-                  Type
-                  <Field
-                    as='select'
-                    id='type'
-                    name='type'
-                    className='flex w-full'
-                    placeholder='Select Seat Type'
-                  >
-                    <option value='default' disabled hidden>
-                      Select Seat Type
-                    </option>
-                    <option value='1'>Carrel Desk</option>
-                    <option value='2'>Carrel Desk with Outlet</option>
-                  </Field>
-                </div>
-                <div>
-                  Description
-                  <Field
-                    as='textarea'
-                    type='textarea'
-                    id='description'
-                    name='description'
-                    className='flex w-full h-32'
-                    style={{ resize: 'none' }}
-                    placeholder='Enter Seat Description'
-                  />
-                </div>
+          <Form className='flex flex-col items-center w-full mb-12 gap-y-7'>
+            <div className='w-full'>
+              <div>
+                <p>Seat ID</p>
+                <Field
+                  type='text'
+                  id='inputSeatID'
+                  name='inputSeatID'
+                  placeholder='Text'
+                  className='bg-iron'
+                  disabled
+                />
+              </div>
+              <div>
+                Name
+                <Field
+                  type='text'
+                  id='name'
+                  name='name'
+                  placeholder='Enter Seat Name'
+                  className='flex w-full'
+                />
+              </div>
+              <div>
+                Type
+                <Field
+                  as='select'
+                  id='type'
+                  name='type'
+                  className='flex w-full'
+                  placeholder='Select Seat Type'
+                >
+                  <option value='default' disabled hidden>
+                    Select Seat Type
+                  </option>
+                  <option value='1'>Carrel Desk</option>
+                  <option value='2'>Carrel Desk with Outlet</option>
+                </Field>
+              </div>
+              <div>
+                Description
+                <Field
+                  as='textarea'
+                  type='textarea'
+                  id='description'
+                  name='description'
+                  className='flex w-full h-32'
+                  style={{ resize: 'none' }}
+                  placeholder='Enter Seat Description'
+                />
               </div>
             </div>
           </Form>

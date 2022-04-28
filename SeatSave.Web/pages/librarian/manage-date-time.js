9795d00e5b69c97b6ae635197075e2d38fe1743e
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-28 23:40:10 +0800

    added functions to call end points

--- SeatSave.Web/pages/librarian/manage-date-time.js
+++ SeatSave.Web/pages/librarian/manage-date-time.js
@@ -3,6 +3,72 @@ import AvailabilitySelectionPanel from '../../components/librarian/manage-date-t
 import PeriodSelectionPanel from '../../components/librarian/manage-date-time/PeriodSelectionPanel';
 
 export default function ManageDateTime() {
+  async function getSpecificDays() {
+    const response = await fetch(
+      `${process.env.API_URL}/Api/Availability/SpecificDay`,
+      {
+        method: 'GET',
+      },
+    );
+    const data = response.json();
+    return data;
+  }
+
+  async function getRegularDayAvailability(dayOfWeek) {
+    const response = await fetch(
+      `${process.env.API_URL}/Api/Availability/RegularDay/${dayOfWeek}`,
+      {
+        method: 'GET',
+      },
+    );
+    const data = response.json();
+    return data;
+  }
+
+  async function getSpecificDayAvailability(isoDate) {
+    const response = await fetch(
+      `${process.env.API_URL}/Api/Availability/RegularDay/${isoDate}`,
+      {
+        method: 'GET',
+      },
+    );
+    const data = response.json();
+    return data;
+  }
+
+  async function deleteSpecificDay(isoDate) {
+    const response = await fetch(
+      `${process.env.API_URL}/Api/Availability/SpecificDay/${isoDate}`,
+      {
+        method: 'DELETE',
+      },
+    );
+    return response.ok;
+  }
+
+  async function updateRegularDayAvailability(dayOfWeek, availability) {
+    const response = await fetch(
+      `${process.env.API_URL}/Api/Availability/RegularDay/${dayOfWeek}`,
+      {
+        method: 'PUT',
+        body: availability,
+      },
+    );
+
+    return response.ok;
+  }
+
+  async function updateSpecificDayAvailability(isoDate, availability) {
+    const response = await fetch(
+      `${process.env.API_URL}/Api/Availability/SpecificDay/${isoDate}`,
+      {
+        method: 'PUT',
+        body: availability,
+      },
+    );
+    return response.ok;
+  }
+
   return (
     <div className='page-container-small'>
       <h1 className='mb-6'>Manage Date and Time</h1>

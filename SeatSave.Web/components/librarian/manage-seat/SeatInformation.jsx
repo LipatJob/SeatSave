15147723acfd410786efe4e350774db45c2d64f8
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-28 22:30:19 +0800

    Get data from API

--- SeatSave.Web/components/librarian/manage-seat/SeatInformation.jsx
+++ SeatSave.Web/components/librarian/manage-seat/SeatInformation.jsx
@@ -1,13 +1,10 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React, { useState, useEffect } from 'react';
 
 export default function SeatInformation(selectedSeatID) {
   // eslint-disable-next-line react/destructuring-assignment
   const currentSeatID = selectedSeatID.selectedSeatID;
   const [userData, setUserData] = useState({});
-  let seatID = 0;
-  let seatName = 'Default';
-  let seatType = 'default';
-  let seatDescription = 'Default';
 
   const getData = async () => {
     const response = await fetch(
@@ -15,72 +12,77 @@ export default function SeatInformation(selectedSeatID) {
     );
     const jsonData = await response.json();
     setUserData(jsonData);
+
+    console.log(jsonData);
   };
   useEffect(() => {
     if (currentSeatID !== 0) getData();
   }, []);
 
-  if (currentSeatID === 0) {
-    seatID = '';
-    seatName = '';
-    seatType = 'default';
-    seatDescription = '';
-  } else {
-    seatID = currentSeatID;
-    seatName = userData.name;
-    seatType = userData.type;
-    seatDescription = userData.description;
-  }
   return (
     <form>
-      <div className='w-full'>
-        <label htmlFor='inputSeatID'>
-          Seat ID
-          <input
-            type='text'
-            id='inputSeatID'
-            placeholder='Text'
-            className='flex bg-iron'
-            value={seatID}
-            disabled
-          />
-        </label>
-        <label htmlFor='inputName'>
-          Name
-          <input
-            type='text'
-            id='inputName'
-            placeholder='Enter Seat Name'
-            className='flex w-full'
-            defaultValue={seatName}
-          />
-        </label>
-        <label htmlFor='ddlType'>
-          Type
-          <select
-            id='ddlType'
-            className='flex w-full'
-            placeholder='Select Seat Type'
-            defaultValue={seatType}
-          >
-            <option value='default' disabled hidden>
-              Select Seat Type
-            </option>
-            <option value='1'>Carrel Desk</option>
-            <option value='2'>Carrel Desk with Outlet</option>
-          </select>
-        </label>
-        <label htmlFor='textareaDescription'>
-          Description
-          <textarea
-            id='textareaDescription'
-            className='flex w-full h-32'
-            style={{ resize: 'none' }}
-            placeholder='Enter Seat Description'
-            defaultValue={seatDescription}
-          />
-        </label>
-      </div>
+      <Formik initialValues={userData} enableReinitialize>
+        {() => (
+          <Form>
+            <div className='sm:max-w-md'>
+              <div className='flex flex-col items-center mb-12 gap-y-7'>
+                <div className='w-full'>
+                  <div>
+                    Seat ID
+                    <Field
+                      type='text'
+                      id='inputSeatID'
+                      name='inputSeatID'
+                      placeholder='Text'
+                      className='flex bg-iron'
+                      disabled
+                    />
+                  </div>
+                  <div>
+                    Name
+                    <Field
+                      type='text'
+                      id='name'
+                      name='name'
+                      placeholder='Enter Seat Name'
+                      className='flex w-full'
+                    />
+                  </div>
+                  <div>
+                    Type
+                    <Field
+                      as='select'
+                      id='type'
+                      name='type'
+                      className='flex w-full'
+                      placeholder='Select Seat Type'
+                    >
+                      <option value='default' disabled hidden>
+                        Select Seat Type
+                      </option>
+                      <option value='1'>Carrel Desk</option>
+                      <option value='2'>Carrel Desk with Outlet</option>
+                    </Field>
+                  </div>
+                  <div>
+                    Description
+                    <Field
+                      as='textarea'
+                      type='textarea'
+                      id='description'
+                      name='description'
+                      className='flex w-full h-32'
+                      style={{ resize: 'none' }}
+                      placeholder='Enter Seat Description'
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <div className='w-full' />
     </form>
   );
 }

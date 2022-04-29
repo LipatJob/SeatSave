b281f06d461f7984b085db187af7c75734c4a322
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-29 20:11:28 +0800

    simplified seeding process

---
+++ SeatSave.Api/Converters/DateOnlyJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace SeatSave.Api.Converters
+{
+    public sealed class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString("O"));
+        }
+
+        public override DateOnly ReadJson(JsonReader reader, Type objectType, DateOnly existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            return DateOnly.FromDateTime(reader.ReadAsDateTime().Value);
+        }
+    }
+
+}
---
+++ SeatSave.EF/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SeatSave.EF;
+
+if (args.Length == 1 && args[0].ToLower() == "seed")
+{
+    DbContextOptionsBuilder options = new DbContextOptionsBuilder();
+    options.UseLazyLoadingProxies();
+    var folder = Environment.SpecialFolder.Personal;
+    var path = Environment.GetFolderPath(folder);
+    var DbPath = System.IO.Path.Join(path, "SeatSave.db");
+    options.UseSqlite(
+        $"Data Source={DbPath}",
+        e => e.MigrationsAssembly("SeatSave.Api"));
+    using (var dbContext = new SeatSaveContext(options.Options))
+    {
+        var seeder = new SeatSaveDbSeeder(dbContext);
+        seeder.Seed();
+    }
+}

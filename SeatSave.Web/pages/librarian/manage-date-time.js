5c22ae798afa1e190ef8adcac41a5e718e1c8a4e
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-30 07:10:31 +0800

    selected period is now gray added code to fetch all periods and override day

--- SeatSave.Web/pages/librarian/manage-date-time.js
+++ SeatSave.Web/pages/librarian/manage-date-time.js
@@ -70,38 +70,63 @@ export default function ManageDateTime() {
     return response.ok;
   }
 
+  async function getAllPeriods() {
+    const response = await fetch(
+      `${process.env.API_URL}/Api/Schedule/Periods`,
+      {
+        method: 'GET',
+      },
+    );
+    const data = await response.json();
+    return data;
+  }
+
   const [pageState, setPageState] = useState({
     availabilityType: 'RegularHours',
     selectedId: '',
-    availability: [],
+    selectedPeriods: [],
     overrideDayItems: [
       {
         id: '2022-01-02',
         name: 'January 01, 2022',
       },
     ],
+    allPeriods: [],
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // setPageState((oldState) => ({
+      //   ...oldState,
+      //   overrideDayItems: getSpecificDays(),
+      //   allPeriods: getAllPeriods()
+      // }));
+    };
+    fetchData().catch(console.error);
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       if (pageState.selectedId === '') return;
 
-      let availability = null;
+      let selectedPeriods = null;
       if (pageState.availabilityType === 'RegularHours') {
-        availability = await getRegularDayAvailability(pageState.selectedId);
+        selectedPeriods = await getRegularDayAvailability(pageState.selectedId);
       } else if (pageState.availabilityType === 'OverrideDays') {
-        availability = await getSpecificDayAvailability(pageState.selectedId);
+        selectedPeriods = await getSpecificDayAvailability(
+          pageState.selectedId,
+        );
       }
 
-      setPageState((oldState) => ({ ...oldState, availability }));
+      setPageState((oldState) => ({ ...oldState, selectedPeriods }));
     };
     fetchData().catch(console.error);
   }, [pageState.selectedId]);
 
   return (
     <div className='page-container-small'>
-      <h1 className='mb-6'>Manage Date and Time</h1>
-      <div className='grid grid-cols-3 gap-x-7'>
+      <h1 className='mb-6'>Manage Date & Time</h1>
+      <div className='grid grid-cols-3 gap-x-7 min-h-[500px]'>
         <AvailabilitySelectionPanel
           pageState={pageState}
           setPageState={setPageState}

3f6f1309543829a0ae24900bd1d5f2de65ebbc9f
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-01 10:35:19 +0800

    fixed data not submitting on first click

--- SeatSave.Web/pages/librarian/manage-seats.js
+++ SeatSave.Web/pages/librarian/manage-seats.js
@@ -108,9 +108,7 @@ export default function ManageSeats({ seats }) {
                   setSeatName={setSeatName}
                   deletionConfirmation={deletionConfirmation}
                   setDeletionConfirmation={setDeletionConfirmation}
-                >
-                  {' '}
-                </SeatInformation>
+                />
               }
             />
           )}

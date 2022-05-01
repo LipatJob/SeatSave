104787a330f3492868ef949064b7ef207259e3ad
charmaine <charmaine.rabano@gmail.com>
2022-05-01 23:27:00 +0800

    Implemented present bookings

--- SeatSave.Web/components/librarian/check-in-out/PresentBookingsSection.jsx
+++ SeatSave.Web/components/librarian/check-in-out/PresentBookingsSection.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { formatTime } from '../../../lib/DateHelper';
 import PreviewBookingsTable from './PreviewBookingsTable';
 
-export default function PresentBookingsSection({ bookings, previewDetails }) {
-  const presentPeriodString = '10:00 am to 11:30 am';
+export default function PresentBookingsSection({
+  period,
+  bookings,
+  previewDetails,
+}) {
+  const presentPeriodString =
+    period != null
+      ? `${formatTime(period.timeStart)} to ${formatTime(period.timeEnd)}`
+      : '';
 
   return (
     <div className='mt-8'>
-      {bookings.length > 0 ? (
-        <div>
-          <h4>Bookings for {presentPeriodString}</h4>
-          <PreviewBookingsTable
-            bookings={bookings}
-            previewDetails={previewDetails}
-          />
-        </div>
-      ) : (
-        <h4>No bookings for {presentPeriodString}</h4>
-      )}
+      {period != null &&
+        (bookings.length > 0 ? (
+          <div>
+            <h4>Bookings for {presentPeriodString}</h4>
+            <PreviewBookingsTable
+              bookings={bookings}
+              previewDetails={previewDetails}
+            />
+          </div>
+        ) : (
+          <h4>No bookings for {presentPeriodString}</h4>
+        ))}
     </div>
   );
 }

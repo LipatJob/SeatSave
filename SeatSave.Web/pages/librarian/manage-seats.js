35ee7b5a4dfe8d928d388e1bf5276802e34c70c2
zoloenma <migalang@live.mcl.edu.ph>
2022-05-01 12:04:27 +0800

    Delete Modal

--- SeatSave.Web/pages/librarian/manage-seats.js
+++ SeatSave.Web/pages/librarian/manage-seats.js
@@ -3,10 +3,9 @@ import Image from 'next/image';
 
 import PanelWithHeader from '../../components/librarian/manage-seat/PanelWithHeader';
 import SeatInformation from '../../components/librarian/manage-seat/SeatInformation';
-import Seat from '../../components/librarian/manage-seat/Seat';
-import DeleteConfirmationModal from '../../components/common/DeleteConfirmationModal';
+
 import AddedSeatModal from '../../components/librarian/manage-seat/AddedSeatModal';
-import CircularButton from '../../components/common/CircularButton';
+
 import SeatSelectionPanel from '../../components/librarian/manage-seat/SeatSelectionPanel';
 
 export default function ManageSeats() {
@@ -15,10 +14,7 @@ export default function ManageSeats() {
   const [seatData, seatSeatData] = useState();
   const [seats, setSeats] = useState([]);
 
-  const [showModalDeleteSeat, setShowModalDeleteSeat] = useState(false);
   const [showModalAddedSeat, setShowModalAddedSeat] = useState(false);
-
-  const [deletionConfirmation, setDeletionConfirmation] = useState(false);
 
   const updateSeats = async () => {
     const res = await fetch(`${process.env.API_URL}/Api/Seats`);
@@ -63,14 +59,6 @@ export default function ManageSeats() {
   const [seatName, setSeatName] = useState();
   return (
     <div className='page-container '>
-      {showModalDeleteSeat && (
-        <DeleteConfirmationModal
-          text='Are you sure you want to delete this seat?'
-          onYes={() => setDeletionConfirmation(true)}
-          onNo={() => setShowModalDeleteSeat(false)}
-          onClose={() => setShowModalDeleteSeat(false)}
-        />
-      )}
       {showModalAddedSeat && (
         <AddedSeatModal onClick={setShowModalAddedSeat} name={seatName} />
       )}
@@ -107,12 +95,9 @@ export default function ManageSeats() {
               body={
                 <SeatInformation
                   seatData={seatData}
-                  setShowModalDeleteSeat={setShowModalDeleteSeat}
                   setFormPart={setFormPart}
                   setShowModalAddedSeat={setShowModalAddedSeat}
                   setSeatName={setSeatName}
-                  deletionConfirmation={deletionConfirmation}
-                  setDeletionConfirmation={setDeletionConfirmation}
                   onAvailableSeatsUpdated={updateAvailableSeats}
                 />
               }

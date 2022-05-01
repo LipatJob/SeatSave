65e3f5f199548dc7d8f5ccb9f0d6474a6ad00a63
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-01 16:29:39 +0800

    implemented modals in home page

--- SeatSave.Web/components/home/CheckedIn.jsx
+++ SeatSave.Web/components/home/CheckedIn.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import BookingDetails from './BookingDetails';
+import OkModal from '../common/OkModal';
 
 export default function CheckedIn() {
+  const [checkoutMessageVisible, setCheckoutMessageVisible] = useState(false);
+  const onCheckOutClicked = () => {
+    setCheckoutMessageVisible(true);
+  };
+  const onCheckoutMessageSeen = () => {
+    setCheckoutMessageVisible(false);
+  };
+
   return (
     <div>
+      {checkoutMessageVisible && (
+        <OkModal
+          onOk={onCheckoutMessageSeen}
+          onClose={onCheckoutMessageSeen}
+          message={
+            <div>
+              <h4 className='mb-6'>See you next time!</h4>
+              <p className='body-normal'>
+                You have checked out of your booking. You can now book a seat
+                for your next visit.
+              </p>
+            </div>
+          }
+        />
+      )}
+
       <div className='grid grid-cols-2 page-container-small'>
         <div className='flex flex-col items-start gap-8'>
           <h1 className='pr-2 text-dusk-blue'>
@@ -13,7 +38,11 @@ export default function CheckedIn() {
           <BookingDetails />
           <div className='flex flex-col items-center gap-4 mt-6'>
             <h4 className='text-center'>Are you leaving CLIR?</h4>
-            <button className='w-full px-16 button' type='button'>
+            <button
+              className='w-full px-16 button'
+              type='button'
+              onClick={onCheckOutClicked}
+            >
               CHECK OUT
             </button>
           </div>

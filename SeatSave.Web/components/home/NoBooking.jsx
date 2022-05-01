1c63117fa40e7ea84f09cd7613097dc5bbf5da3f
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-02 07:56:42 +0800

    mobile UI for NoBooking

--- SeatSave.Web/components/home/NoBooking.jsx
+++ SeatSave.Web/components/home/NoBooking.jsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 
 export default function NoBooking() {
   return (
-    <div className='grid grid-cols-2 page-container-small'>
+    <div className='grid grid-cols-1 gap-20 sm:grid-cols-2 page-container-small'>
       <div className='flex flex-col items-start gap-24'>
         <h1 className='mt-20 text-dusk-blue'>
           You have no booking at the moment...
         </h1>
         <Link href='/book-a-seat'>
-          <button type='button' className='w-96 button'>
+          <button type='button' className='w-full sm:w-96 button'>
             BOOK A SEAT
           </button>
         </Link>
@@ -18,7 +18,7 @@ export default function NoBooking() {
       <div className='relative'>
         <Image
           src='/NoBookingDecoration.svg'
-          className='relative w-full h-auto'
+          className='w-full h-auto'
           layout='responsive'
           objectFit='contain'
           width={800}

c1b866e6d6add51e2ec3957760a43e9b88d70ccb
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-01 15:17:13 +0800

    initial code for fetching data from api

--- SeatSave.Web/pages/index.js
+++ SeatSave.Web/pages/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CheckedIn from '../components/home/CheckedIn';
 import NoBooking from '../components/home/NoBooking';
 import PendingBooking from '../components/home/PendingBooking';
 
 export default function ViewBookingDetails() {
+  const [currentBooking, setCurrentBooking] = useState({});
+  const fetchCurrentBooking = async () => {
+    const response = await fetch(`${process.env.API_URL}/Api/Booking/Current`);
+
+    if (response.status !== 200) {
+      console.log('An error occured trying to fetch your booking details');
+    }
+    const data = response.json();
+
+    console.log(data);
+    return data;
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       {/* <NoBooking /> */}

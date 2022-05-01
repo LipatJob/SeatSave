1a6abc2ff422a64279efab7ebbf303aea758c0cc
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-01 13:38:43 +0800

    implemented NoBooking UI

--- SeatSave.Web/components/home/NoBooking.jsx
+++ SeatSave.Web/components/home/NoBooking.jsx
@@ -1,14 +1,24 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function NoBooking() {
   return (
-    <div>
-      <div>You have no booking at the moment...</div>
+    <div className='grid grid-cols-2 page-container-small'>
+      <div className='flex flex-col items-start gap-24'>
+        <h1 className='mt-20 text-dusk-blue'>
+          You have no booking at the moment...
+        </h1>
+        <Link href='/book-a-seat'>
+          <button type='button' className='w-96 button'>
+            BOOK A SEAT
+          </button>
+        </Link>
+      </div>
       <div>
         <Image
           src='/NoBookingDecoration.svg'
-          className='w-full h-auto'
+          className='relative w-full h-auto'
           layout='responsive'
           objectFit='contain'
           width={500}

eff658d357f48e82ecd4ebd77e0915f682d4e96f
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-02 08:19:00 +0800

    adjusted responsiveness of modals

--- SeatSave.Web/components/common/OkModal.jsx
+++ SeatSave.Web/components/common/OkModal.jsx
@@ -4,7 +4,7 @@ import ModalBase from './ModalBase';
 export default function OkModal({ onOk, onClose, message }) {
   return (
     <ModalBase onClose={onClose}>
-      <div className=' w-[500px] h-[300px] flex flex-col items-center px-6'>
+      <div className='flex flex-col items-center px-6 '>
         <h4 className='mx-auto mb-8 text-center'>{message}</h4>
         <button type='button' className='px-24 button' onClick={onOk}>
           OK
--- SeatSave.Web/components/common/WarningConfirmationModal.jsx
+++ SeatSave.Web/components/common/WarningConfirmationModal.jsx
@@ -9,16 +9,16 @@ export default function WarningConfirmationModal({
 }) {
   return (
     <ModalBase onClose={onClose}>
-      <div className='relative flex-auto w-[500px] h-[300px]'>
+      <div className='relative flex-auto'>
         <div className='w-full h-full p-4 text-4xl text-center'>
           <p className='mb-4'>{text}</p>
           <div className='w-full py-4 pt-4 text-4xl text-center'>
-            <button type='button' className='w-2/5 red-button' onClick={onYes}>
+            <button type='button' className='w-48 red-button' onClick={onYes}>
               YES
             </button>
           </div>
           <div className='w-full text-4xl text-center'>
-            <button type='button' className='w-2/5 gray-button' onClick={onNo}>
+            <button type='button' className='w-48 gray-button' onClick={onNo}>
               NO
             </button>
           </div>

1c86dca9e5f086273eafaa1bab233dbbe3e6278e
charmaine <charmaine.rabano@gmail.com>
2022-05-03 21:16:10 +0800

    Started improvements

--- SeatSave.Web/components/common/navbar/DesktopMenu.jsx
+++ SeatSave.Web/components/common/navbar/DesktopMenu.jsx
@@ -3,10 +3,14 @@ import Link from 'next/link';
 
 export default function DesktopMenu({ links }) {
   return (
-    <div className='flex flex-row gap-10 body-small'>
+    <div className='flex flex-row items-center gap-10 body-small'>
       {links.map((link) => (
         <Link href={link.path} key={link.key}>
-          {link.name}
+          {link.key === 3 ? (
+            <button type='button' className='button hover:bg-white hover:text-bluish'>{link.name}</button>
+          ) : (
+            link.name
+          )}
         </Link>
       ))}
     </div>
--- SeatSave.Web/components/common/navbar/LibrarianNavbar.jsx
+++ SeatSave.Web/components/common/navbar/LibrarianNavbar.jsx
@@ -1,49 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
 import NavbarLogo from './NavbarLogo';
 import DesktopMenu from './DesktopMenu';
 import AccountButton from './AccountButton';
-import MobileMenu from './MobileMenu';
+import MobileMenuOverlay from './MobileMenuOverlay';
 import librarianAuthService from '../../../lib/librarianAuthService';
 
 const librarianLinks = [
   {
     name: 'Check In / Out',
     path: '/librarian/',
+    key: 3,
   },
   {
     name: 'View Bookings',
     path: '/librarian/view-bookings',
+    key: 4,
   },
   {
     name: 'Manage Seats',
     path: '/librarian/manage-seats',
+    key: 5,
   },
   {
     name: 'Manage Date & Time',
     path: '/librarian/manage-date-time',
+    key: 6,
   },
 ];
 
 export default function LibrarianNavbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
-    <div className='fixed z-50 flex flex-col justify-center w-full h-20 bg-dusk-blue'>
-      <div className='flex flex-row items-center justify-between w-full text-white page-container'>
-        <div className='flex flex-row items-center'>
-          <NavbarLogo />
-          <div className='hidden ml-12 lg:block'>
-            <DesktopMenu links={librarianLinks} />
+    <>
+      <div className='fixed z-50 flex flex-col justify-center w-full h-20 bg-dusk-blue'>
+        <div className='flex flex-row items-center justify-between w-full text-white page-container'>
+          <div className='flex flex-row items-center'>
+            <NavbarLogo />
+            <div className='hidden ml-12 lg:block'>
+              <DesktopMenu links={librarianLinks} />
+            </div>
+          </div>
+          <div className='hidden lg:block'>
+            <AccountButton authService={librarianAuthService} />
+          </div>
+          <div className='lg:hidden'>
+            <button type='button' onClick={() => setNavbarOpen(!navbarOpen)}>
+              {navbarOpen ? <HiOutlineX /> : <HiOutlineMenu />}
+            </button>
           </div>
         </div>
-        <div className='hidden lg:block'>
-          <AccountButton authService={librarianAuthService} />
-        </div>
-        <div className='lg:hidden'>
-          <MobileMenu
-            links={librarianLinks}
-            onLogout={librarianAuthService.logout}
-          />
-        </div>
       </div>
-    </div>
+      <MobileMenuOverlay
+        navbarOpen={navbarOpen}
+        setNavbarOpen={setNavbarOpen}
+        links={librarianLinks}
+        onLogout={librarianAuthService.logout}
+      />
+    </>
   );
 }

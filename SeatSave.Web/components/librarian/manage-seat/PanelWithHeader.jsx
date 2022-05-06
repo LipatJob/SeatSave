8e608a3dfad8818c11b27bfbc0841a1dd93f834b
zoloenma <migalang@live.mcl.edu.ph>
2022-05-06 23:13:25 +0800

    HL-140

--- SeatSave.Web/components/librarian/manage-seat/PanelWithHeader.jsx
+++ SeatSave.Web/components/librarian/manage-seat/PanelWithHeader.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export default function PanelWithHeader({ header, body }) {
+export default function PanelWithHeader({ header, body, className }) {
   return (
-    <div>
+    <div className={className}>
       <div className='w-full shadow-lg'>
         <div className='flex w-full text-black bg-pearl-bush'>
-          <div className='py-4 pl-4'>
-            <h4>{header}</h4>
-          </div>
+          <div className='w-full py-4 pl-4'>{header}</div>
         </div>
         <div>
           <div className='px-4 py-4 h-content lg:h-[550px]'>{body}</div>
--- SeatSave.Web/pages/librarian/manage-seats.js
+++ SeatSave.Web/pages/librarian/manage-seats.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { GrClose } from 'react-icons/gr';
 import PanelWithHeader from '../../components/librarian/manage-seat/PanelWithHeader';
 import SeatInformation from '../../components/librarian/manage-seat/SeatInformation';
 import OkModal from '../../components/common/OkModal';
@@ -49,45 +50,51 @@ export default function ManageSeats() {
       <div className='pb-4 h-fit '>
         <h1>Manage Seats</h1>
       </div>
-      <div className='grid grid-cols-1 gap-8 lg:grid-cols-3'>
-        <div id='leftPanel' className='lg:col-span-1'>
-          <SeatSelectionPanel
-            seats={seats}
-            onAddClicked={() => {
-              setCurrentID(0);
-              setFormPart(1);
-            }}
-            onSeatSelected={(id) => {
-              setCurrentID(id);
-              setFormPart(1);
-            }}
-          />
-        </div>
-        <div id='rightPanel' className=' lg:col-span-2'>
-          {formPart === 0 && (
-            <div className='max-h-[570px] w-full text-center pt-10 hidden sm:block'>
-              <Image
-                src='/ManageSeatsDecoration.png'
-                width={500}
-                height={500}
+      <div className='relative md:grid md:gap-8 md:grid-cols-3'>
+        <SeatSelectionPanel
+          seats={seats}
+          onAddClicked={() => {
+            setCurrentID(0);
+            setFormPart(1);
+          }}
+          onSeatSelected={(id) => {
+            setCurrentID(id);
+            setFormPart(1);
+          }}
+        />
+        {formPart === 0 && (
+          <div className='hidden w-full h-full pt-10 text-center md:col-span-2 md:block'>
+            <Image src='/ManageSeatsDecoration.png' width={500} height={500} />
+          </div>
+        )}
+        {formPart === 1 && (
+          <PanelWithHeader
+            className='absolute top-0 w-full h-full bg-white md:col-span-2 md:top-auto md:relative'
+            header={
+              <div className='flow-root'>
+                <h4 className='float-left '> Seat Information</h4>
+                <span className='float-right pt-2 pr-4 md:hidden'>
+                  <button
+                    type='button'
+                    onClick={() => setFormPart(0)}
+                    className='ml-auto'
+                  >
+                    <GrClose className='mx-auto my-auto' />
+                  </button>
+                </span>
+              </div>
+            }
+            body={
+              <SeatInformation
+                setFormPart={setFormPart}
+                setShowModalAddedSeat={setShowModalAddedSeat}
+                setSeatName={setSeatName}
+                onAvailableSeatsUpdated={updateAvailableSeats}
+                currentID={currentID}
               />
-            </div>
-          )}
-          {formPart === 1 && (
-            <PanelWithHeader
-              header='Seat Information'
-              body={
-                <SeatInformation
-                  setFormPart={setFormPart}
-                  setShowModalAddedSeat={setShowModalAddedSeat}
-                  setSeatName={setSeatName}
-                  onAvailableSeatsUpdated={updateAvailableSeats}
-                  currentID={currentID}
-                />
-              }
-            />
-          )}
-        </div>
+            }
+          />
+        )}
       </div>
     </div>
   );

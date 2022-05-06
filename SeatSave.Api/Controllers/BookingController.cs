eaa8ae0bee6594fe3ec059af2a1ea56b5b0c5a89
charmaine <charmaine.rabano@gmail.com>
2022-05-06 15:26:04 +0800

    Implemented differentiate bookings by status

--- SeatSave.Api/Controllers/BookingController.cs
+++ SeatSave.Api/Controllers/BookingController.cs
@@ -157,7 +157,7 @@ namespace SeatSave.Api.Controllers
                                 (code == null || b.BookingCode == code) &&
                                 (status == null || b.Status == status) &&
                                 (date == null || b.BookingDate == bookingDate) &&
-                                (email == null || b.VisitorModel.Email.ToLower() == email.ToLower())                                
+                                (email == null || b.VisitorModel.Email.ToLower() == email.ToLower())
                                 )
                             .OrderByDescending(b => b.Id);
 
@@ -171,7 +171,12 @@ namespace SeatSave.Api.Controllers
             var currentDate = DateOnly.FromDateTime(currentDateTime);
             var currentTime = new TimeOnly(currentDateTime.Hour, currentDateTime.Minute, currentDateTime.Second);
 
-            return Ok(dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime));
+            var pendingBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.PendingStatus);
+            var checkedInBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.CheckedInStatus);
+            var checkedOutBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.CheckedOutStatus);
+            var cancelledBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.CancelledStatus);
+
+            return Ok(pendingBookings.Concat(checkedInBookings).Concat(checkedOutBookings).Concat(cancelledBookings));
         }
 
     }

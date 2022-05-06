744ab7d3e1d22d6ca0cba6d93ed1a06a3a6eddc9
charmaine <charmaine.rabano@gmail.com>
2022-05-06 13:35:18 +0800

    Reverted LibrarianNavbar.jsx

--- SeatSave.Web/components/common/navbar/LibrarianNavbar.jsx
+++ SeatSave.Web/components/common/navbar/LibrarianNavbar.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
+import React from 'react';
 import NavbarLogo from './NavbarLogo';
 import DesktopMenu from './DesktopMenu';
 import AccountButton from './AccountButton';
-import MobileMenuOverlay from './MobileMenuOverlay';
+import MobileMenu from './MobileMenu';
 import librarianAuthService from '../../../lib/librarianAuthService';
 
 const librarianLinks = [
@@ -30,34 +29,25 @@ const librarianLinks = [
 ];
 
 export default function LibrarianNavbar() {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-
   return (
-    <>
-      <div className='fixed z-50 flex flex-col justify-center w-full h-20 bg-dusk-blue'>
-        <div className='flex flex-row items-center justify-between w-full text-white page-container'>
-          <div className='flex flex-row items-center'>
-            <NavbarLogo />
-            <div className='hidden ml-12 lg:block'>
-              <DesktopMenu links={librarianLinks} />
-            </div>
-          </div>
-          <div className='hidden lg:block'>
-            <AccountButton authService={librarianAuthService} />
-          </div>
-          <div className='lg:hidden'>
-            <button type='button' onClick={() => setNavbarOpen(!navbarOpen)}>
-              {navbarOpen ? <HiOutlineX /> : <HiOutlineMenu />}
-            </button>
+    <div className='fixed z-50 flex flex-col justify-center w-full h-20 bg-dusk-blue'>
+      <div className='flex flex-row items-center justify-between w-full text-white page-container'>
+        <div className='flex flex-row items-center'>
+          <NavbarLogo />
+          <div className='hidden ml-12 lg:block'>
+            <DesktopMenu links={librarianLinks} />
           </div>
         </div>
+        <div className='hidden lg:block'>
+          <AccountButton authService={librarianAuthService} />
+        </div>
+        <div className='lg:hidden'>
+          <MobileMenu
+            links={librarianLinks}
+            onLogout={librarianAuthService.logout}
+          />
+        </div>
       </div>
-      <MobileMenuOverlay
-        navbarOpen={navbarOpen}
-        setNavbarOpen={setNavbarOpen}
-        links={librarianLinks}
-        onLogout={librarianAuthService.logout}
-      />
-    </>
+    </div>
   );
 }

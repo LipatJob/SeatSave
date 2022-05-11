caec1fe3ecfc51592466ab8a44ae1ea1c8d4fc37
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-11 09:14:05 +0800

    implemented delete

--- SeatSave.Web/components/seat-map/TrashCan.jsx
+++ SeatSave.Web/components/seat-map/TrashCan.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Image } from 'react-konva';
 import useImage from 'use-image';
 
-export default function TrashCan() {
+export default function TrashCan({ x, y, ref }) {
   const [image] = useImage('./TrashCan.svg');
 
-  return <Image image={image} width={50} height={50} />;
+  return <Image image={image} width={50} height={50} x={x} y={y} ref={ref} />;
 }

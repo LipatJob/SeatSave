d280aeef89c4de726ce54c62f07913a2692ebe9c
charmaine <charmaine.rabano@gmail.com>
2022-05-13 21:29:04 +0800

    Implemented hover and active nav links

--- SeatSave.Web/components/common/navbar/DesktopMenu.jsx
+++ SeatSave.Web/components/common/navbar/DesktopMenu.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function DesktopMenu({ links }) {
+  const router = useRouter();
+
   return (
     <div className='flex flex-row items-center gap-10 body-small'>
       {links.map((link) => (
         <Link href={link.path} key={link.key}>
           {link.key === 3 ? (
-            <button type='button' className='button hover:bg-white hover:text-bluish'>{link.name}</button>
+            <button
+              type='button'
+              className={`button hover:bg-white hover:text-bluish ${
+                router.pathname === '/librarian' ? 'bg-white text-bluish' : ''
+              }`}
+            >
+              {link.name}
+            </button>
           ) : (
-            link.name
+            <button
+              type='button'
+              className={`hover:border-b ${
+                router.pathname === link.path ? 'border-b' : ''
+              }`}
+            >
+              {link.name}
+            </button>
           )}
         </Link>
       ))}

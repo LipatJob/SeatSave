be3d8682902cbfc50da9d886bf3776573ca416db
charmaine <charmaine.rabano@gmail.com>
2022-05-16 09:18:23 +0800

    Implemented top departments report

--- SeatSave.Api/Services/StudentReportService.cs
+++ SeatSave.Api/Services/StudentReportService.cs
@@ -1,24 +1,48 @@
 using SeatSave.Core.Booking;
+using SeatSave.Core.User;
+using SeatSave.EF;
 
 namespace SeatSave.Api.Services
 {
+    public class ReportData
+    {
+        public IEnumerable<string> Categories { get; set; }
+        public IEnumerable<int> Counts { get; set; }
+    }
+
     public class StudentReportService
     {
+        private readonly SeatSaveContext dbContext;
         private readonly DateOnly dateStart;
         private readonly DateOnly dateEnd;
         private readonly IEnumerable<BookingModel> bookings;
 
-        public StudentReportService(DateOnly dateStart, DateOnly dateEnd, IEnumerable<BookingModel> bookings)
+        public StudentReportService(SeatSaveContext dbContext, DateOnly dateStart, DateOnly dateEnd)
         {
+            this.dbContext = dbContext;
             this.dateStart = dateStart;
             this.dateEnd = dateEnd;
-            this.bookings = bookings;
+
+            this.bookings = GetBookingsReportPeriod(dateStart, dateEnd);
         }
 
-        public IEnumerable<string> GetTopDepartments() { throw new NotImplementedException("To be implemented"); }
+        public IEnumerable<ReportData> GetTopDepartments()
+        {
+            var departmentGroups = bookings.Select(b => b.VisitorModel).OfType<Student>().GroupBy(e => e.Department);
+
+            var topDepartmentsData = new List<ReportData>() { new ReportData() {
+                Categories = bookings.Select(b => b.VisitorModel).OfType<Student>().GroupBy(e => e.Department).Select(group => group.Key),
+                Counts = bookings.Select(b => b.VisitorModel).OfType<Student>().GroupBy(e => e.Department).Select(group => group.Count())
+            }};
+
+            return topDepartmentsData;
+        }
         public IEnumerable<string> GetTopProgramStrands() { throw new NotImplementedException("To be implemented"); }
         public IEnumerable<string> GetTopYearLevel() { throw new NotImplementedException("To be implemented"); }
         public IEnumerable<string> GetTopProgramStrandAndYearLevel() { throw new NotImplementedException("To be implemented"); }
-        public IEnumerable<string> GetBookingsReportPeriod() { throw new NotImplementedException("To be implemented"); }
+        public IEnumerable<BookingModel> GetBookingsReportPeriod(DateOnly dateStart, DateOnly dateEnd)
+        {
+            return dbContext.Bookings.Where(b => b.BookingDate >= dateStart && b.BookingDate <= dateEnd);
+        }
     }
 }
--- SeatSave.Web/pages/librarian/generate-reports.js
+++ SeatSave.Web/pages/librarian/generate-reports.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReportSection from '../../components/librarian/generate-reports/ReportSection';
 
 const DEPARTMENTS_NAME = 'Departments';
@@ -6,25 +6,24 @@ const PROGRAMS_NAME = 'Programs/Strands';
 const YEARS_NAME = 'Year Levels';
 const PROGRAMS_YEARS_NAME = 'Programs/Strands and Year Levels';
 
-// dummy data
-const departmentsData = {
-  categories: ['MITL', 'CCIS', 'CAS', 'SHS', 'ETYCB'],
-  series: [{ name: 'Student count', data: [66, 46, 34, 33, 23] }],
-};
-const programsData = {
-  categories: ['CHE', 'CE', 'AR', 'CS', 'COMM'],
-  series: [{ name: 'Student count', data: [32, 22, 21, 20, 16] }],
-};
-const yearsData = {
-  categories: [3, 4, 2, 1, 12],
-  series: [{ name: 'Student count', data: [30, 19, 18, 17, 10] }],
-};
-const programsYearsData = {
-  categories: ['CHE - 3', 'CHE - 2', 'AR - 4', 'CS - 4', 'COMM - 2'],
-  series: [{ name: 'Student count', data: [22, 20, 16, 15, 9] }],
-};
+export default function GenerateReports({ allReports }) {
+  const [departmentsData, setDepartmentsData] = useState({
+    categories: allReports[0].categories,
+    series: [{ name: 'Student count', data: allReports[0].counts }],
+  });
+  const [programsData, setProgramsData] = useState({
+    categories: ['CHE', 'CE', 'AR', 'CS', 'COMM'],
+    series: [{ name: 'Student count', data: [32, 22, 21, 20, 16] }],
+  });
+  const [yearsData, setYearsData] = useState({
+    categories: [3, 4, 2, 1, 12],
+    series: [{ name: 'Student count', data: [30, 19, 18, 17, 10] }],
+  });
+  const [programsYearsData, setProgramsYearsData] = useState({
+    categories: ['CHE - 3', 'CHE - 2', 'AR - 4', 'CS - 4', 'COMM - 2'],
+    series: [{ name: 'Student count', data: [22, 20, 16, 15, 9] }],
+  });
 
-export default function GenerateReports() {
   return (
     <div className='page-container'>
       <h1>Generate Reports</h1>
@@ -71,6 +70,17 @@ export default function GenerateReports() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.API_URL}/Api/StudentReport`);
+  const allReports = await res.json();
+
+  return {
+    props: {
+      allReports,
+    },
+  };
 }
 
 GenerateReports.page = 'Generate Reports';

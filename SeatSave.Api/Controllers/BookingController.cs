4fda13ee250cd40acb363d8487b4fed759d6146c
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-18 01:56:51 +0800

    reinclude migrations

--- SeatSave.Api/Controllers/BookingController.cs
+++ SeatSave.Api/Controllers/BookingController.cs
@@ -207,12 +207,9 @@ namespace SeatSave.Api.Controllers
             var currentDate = DateOnly.FromDateTime(currentDateTime);
             var currentTime = new TimeOnly(currentDateTime.Hour, currentDateTime.Minute, currentDateTime.Second);
 
-            var pendingBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.PendingStatus);
-            var checkedInBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.CheckedInStatus);
-            var checkedOutBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.CheckedOutStatus);
-            var cancelledBookings = dbContext.Bookings.Where(b => b.BookingDate == currentDate && b.Period.TimeStart <= currentTime && b.Period.TimeEnd >= currentTime && b.Status == BookingModel.CancelledStatus);
+            var pendingBookings = dbContext.Bookings.Where(b => b.Period.TimeStart <= currentTime);
 
-            return Ok(pendingBookings.Concat(checkedInBookings).Concat(checkedOutBookings).Concat(cancelledBookings));
+            return Ok(pendingBookings);
         }
 
     }

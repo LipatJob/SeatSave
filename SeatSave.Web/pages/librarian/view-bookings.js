1d137c77e0219522a498b0b948ec7b7a6474f7b7
zoloenma <migalang@live.mcl.edu.ph>
2022-05-17 21:51:01 +0800

    clean code

--- SeatSave.Web/pages/librarian/view-bookings.js
+++ SeatSave.Web/pages/librarian/view-bookings.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-bind */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';

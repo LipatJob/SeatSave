cde0ce3e45b8ac68eb45ad830af8136e22683fb4
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-18 01:56:51 +0800

    reinclude migrations

---
+++ SeatSave.Api/Migrations/20220517173225_Initial2.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeatSave.Api.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Seat_SeatId",
+                table: "Bookings");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Seat",
+                table: "Seat");
+
+            migrationBuilder.RenameTable(
+                name: "Seat",
+                newName: "Seats");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "Seats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PositionX",
+                table: "Seats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PositionY",
+                table: "Seats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Width",
+                table: "Seats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Seats",
+                table: "Seats",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Tables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    PositionX = table.Column<int>(type: "int", nullable: false),
+                    PositionY = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tables", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Seats_SeatId",
+                table: "Bookings",
+                column: "SeatId",
+                principalTable: "Seats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Seats_SeatId",
+                table: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Tables");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Seats",
+                table: "Seats");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "Seats");
+
+            migrationBuilder.DropColumn(
+                name: "PositionX",
+                table: "Seats");
+
+            migrationBuilder.DropColumn(
+                name: "PositionY",
+                table: "Seats");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "Seats");
+
+            migrationBuilder.RenameTable(
+                name: "Seats",
+                newName: "Seat");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Seat",
+                table: "Seat",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Seat_SeatId",
+                table: "Bookings",
+                column: "SeatId",
+                principalTable: "Seat",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

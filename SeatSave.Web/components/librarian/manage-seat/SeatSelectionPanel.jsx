4a535cf428fe34fa61da2d56c940f3b3941f9e25
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-05-24 01:32:11 +0800

    fixed position of circular button in manage seats

--- SeatSave.Web/components/librarian/manage-seat/SeatSelectionPanel.jsx
+++ SeatSave.Web/components/librarian/manage-seat/SeatSelectionPanel.jsx
@@ -15,22 +15,21 @@ export default function SeatSelectionPanel({
       header={<h4>Available Seats</h4>}
       body={
         <div className='h-[436px] overflow-auto'>
-          <div className='flex flex-col max-h-full overflow-hidden overflow-y-scroll '>
-          {seats.map((seat) => (
-            <Seat
-              key={seat.id}
-              Name={seat.name}
-              Code={seat.id}
-              onClick={() => onSeatSelected(seat.id)}
-            />
-          ))}
+          <div className='flex flex-col h-full overflow-hidden overflow-y-scroll '>
+            {seats.map((seat) => (
+              <Seat
+                key={seat.id}
+                Name={seat.name}
+                Code={seat.id}
+                onClick={() => onSeatSelected(seat.id)}
+              />
+            ))}
 
-          <div className='sticky mt-3 bottom-3 right-3'>
-            <CircularButton onClick={onAddClicked} />
+            <div className='sticky mt-auto bottom-3 right-3'>
+              <CircularButton onClick={onAddClicked} />
+            </div>
           </div>
         </div>
-        </div>
-        
       }
     />
   );
